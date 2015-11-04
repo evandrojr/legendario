2e24b4d34e0106e43f5e9baa2c6912628a8fd2ed
Evandro Junior <evandrojr@gmail.com>
2015-11-04 00:22:19 -0300

    working

--- legendario.rb
+++ legendario.rb
@@ -6,21 +6,18 @@ require 'awesome_print'
 require 'file-monitor'
 require 'find'
 
-
 lib_dir = File.join File.dirname(__FILE__), '../lib'
 $:.unshift lib_dir unless $:.include? lib_dir
 
 class Legendario
-
 	@dir = "/home/j/Downloads"
-
 	def lang
-		@lang=[]
-		@lang=["eng", "por", "spa", "ger"]
-		@lang=["eng"]
+		[]
+		["eng", "por", "spa", "ger"]
+#		["eng"]
 	end
 
-	def self.start
+	def self.watch
 		Legendario.new.watch_dirs(@dir)
 	end
 
@@ -33,16 +30,14 @@ class Legendario
 	end
 
 	def download_subs(file)
-		@lang.each do |l|
+		lang().each do |l|
 			puts "Language: #{l}"
-			se("getsub -aLl #{l} #{file}")
+			se("getsub -aLl #{l} \"#{file}\"")
 		end
 		symlink(File.dirname(file))
 	end
 
 	def symlink(dir)
-		total_size = 0
-		total_files = 0
 		Find.find(dir) do |path|
 		  if FileTest.directory?(path)
 		    if File.basename(path)[0] == ?.
@@ -54,20 +49,19 @@ class Legendario
 				if FileTest.symlink?(path)
 					se("rm \"#{path}\"")
 				end
-				@lang.each do |l|
-					if /\.#{l}\.srt$/i =~ File.basename(path)
+				symlinked = false
+				lang().each do |l|
+					if /\.#{l}\.srt$/i =~ File.basename(path) and !symlinked
 						puts path
 						link = path.sub(/\.#{l}\.srt$/i, '.srt')
 						puts link
+						symlinked =  true
 						se("ln -s \"#{path}\" \"#{link}\"")
-			    	total_files +=1
-			   		total_size += FileTest.size(path)
-						break
+						break #this break is not working so using !symlinked
 			    end
 				end
 		  end
 		end
-		puts "Total of files linked: #{total_files} Total of file size of linked files: #{total_size}"
 	end
 
 	def watch_dirs(dir)
@@ -80,22 +74,21 @@ class Legendario
 		    disallow /.*/
 		    allow /\.mkv$|\.mp4$|\.avi$/
 		  }
-
-			#  begin
+			begin
 				exec do |events|
 			    events.each do |ev|
 			      file = File.join(ev.watcher.path(), ev.name())
 						puts file
-						self.download_subs(file)
+						Legendario.new.download_subs(file)
 			    end
 			  end
 				#Avoids breaking after a directory been deleted
-				# rescue => error
-					# puts("#{error.class} and #{error.message}")
-					# Legendario.start
-			# end
+				rescue => error
+					puts("#{error.class} and #{error.message}")
+					Legendario.watch
+			end
 		end
 	end
 end
 
-Legendario.start
+Legendario.watch

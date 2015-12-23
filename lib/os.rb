9909cb939139d6c0df4961cb0ccf71ffe53bf447
Evandro Junior <evandrojr@gmail.com>
2015-12-23 06:51:36 -0300

    fixed testes

--- lib/os.rb
+++ lib/os.rb
@@ -1,3 +1,5 @@
+require 'fileutils'
+
 class Os
 
   class << self
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,6 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+$LOAD_PATH.unshift File.expand_path('../../lib/legendario', __FILE__)
 require 'settings'
-require 'legendario'
 require 'os'
+require 'version'
+require 'sl' 

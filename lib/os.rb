f4e23af9f7c41e91afaf7030cf2a94255268afcc
Evandro Junior <evandrojr@gmail.com>
2015-12-23 05:51:53 -0300

    Support for imdb name search

--- lib/os.rb
+++ lib/os.rb
@@ -35,7 +35,7 @@ class Os
       delete_video_default_subtitle(file)
   		Settings.langs.each do |lang|
   			Sl.info "Language: #{lang}"
-  			se("getsub -aLl #{lang} \"#{file}\"")
+  			se("getsub -s hinp -aLl #{lang} \"#{file}\"")
         # Will to it just for the 1st time
         make_default_subtitle(file, lang)
   		end
